fe85bd6c73f4f87d5ba45e63e86f6fb7bd961356
nterada <n@mount.jp>
2024-12-27 17:47:35 +0900

    スポットライト付かない

--- src/js/three2.ts
+++ src/js/three2.ts
@@ -68,7 +68,7 @@ class ThreeApp {
 
   static DIRECTIONAL_LIGHT_PARAM = {
     color: 0xffffff,                            // 光の色
-    intensity: 4.0,                             // 光の強度
+    intensity: 1.0,                             // 光の強度
     // position: new THREE.Vector3(2.0, 2.0, 2.0), // 光の向き
     position: new THREE.Vector3(20.0, 10.0, 10.0), // 光の向き
   };
@@ -76,7 +76,13 @@ class ThreeApp {
   static AMBIENT_LIGHT_PARAM = {
     // color: 0xffffff, // 光の色
     color: 0xffffff, // 光の色
-    intensity: 0.5,  // 光の強度
+    intensity: 0.1,  // 光の強度
+  };
+
+  static SPOT_LIGHT_PARAM = {
+    // color: 0xffffff, // 光の色
+    color:  0xf0f0f0, // 光の色
+    intensity: 0.1,  // 光の強度
   };
 
 
@@ -87,14 +93,19 @@ class ThreeApp {
   friction: number = 0.95;  // 摩擦（0.95は少しスムーズな減速）
   // friction: number = 0.55;  // 摩擦（0.95は少しスムーズな減速）
 
+  planeGeo;
+  clock;
   box;
   boxArray
   geometry;
+  plane;
   arrowHelper;
   lighthelper;
+  spotlight;
 
 
-  hemisphereLight
+  spotLightHelper;
+  hemisphereLight;
 
 
   renderer;         // レンダラ
@@ -146,26 +157,42 @@ class ThreeApp {
     this.render = this.render.bind(this);
 
     // ディレクショナルライト（平行光源）
-    this.directionalLight = new THREE.DirectionalLight(
-      ThreeApp.DIRECTIONAL_LIGHT_PARAM.color,
-      ThreeApp.DIRECTIONAL_LIGHT_PARAM.intensity
-    );
-    this.directionalLight.position.copy(ThreeApp.DIRECTIONAL_LIGHT_PARAM.position);
-    this.scene.add(this.directionalLight);
+    // this.directionalLight = new THREE.DirectionalLight(
+    //   ThreeApp.DIRECTIONAL_LIGHT_PARAM.color,
+    //   ThreeApp.DIRECTIONAL_LIGHT_PARAM.intensity
+    // );
+    // this.directionalLight.position.copy(ThreeApp.DIRECTIONAL_LIGHT_PARAM.position);
+    // this.scene.add(this.directionalLight);
 
     // アンビエントライト（環境光）
-    this.ambientLight = new THREE.AmbientLight(
-      ThreeApp.AMBIENT_LIGHT_PARAM.color,
-      ThreeApp.AMBIENT_LIGHT_PARAM.intensity,
+    // this.ambientLight = new THREE.AmbientLight(
+    //   ThreeApp.AMBIENT_LIGHT_PARAM.color,
+    //   ThreeApp.AMBIENT_LIGHT_PARAM.intensity,
+    // );
+    // this.scene.add(this.ambientLight);
+
+
+
+    this.spotlight = new THREE.SpotLight(
+      ThreeApp.SPOT_LIGHT_PARAM.color,
+      ThreeApp.SPOT_LIGHT_PARAM.intensity
     );
-    this.scene.add(this.ambientLight);
 
+    this.scene.add(this.spotlight);
+    // this.spotlight.position.set( 10, 20, 20 )
 
-    this.hemisphereLight = new THREE.HemisphereLight(0x507fff, 0xd0e040, 0.3)
+    this.spotlight.position.set( 10, 20, 20 );  // スポットライトの位置
+    this.spotlight.position.set( 0, 10, 0 );  // スポットライトの位置
+    // this.spotlight.target.position.set( 0, 0, 0 );  // スポットライトのターゲット（照らす位置）
+    // this.scene.add(this.spotlight.target);
 
-    this.scene.add(this.hemisphereLight);
+    this.spotlight.angle = Math.PI / 5; // 光の広がり角度（45度）
+    this.spotlight.penumbra = 1;  // 光の端のぼかし具合
+    this.spotlight.decay = 2;  // 減衰
+    this.spotlight.distance = 10;
 
-
+    const spotLightHelper = new THREE.SpotLightHelper( this.spotlight );
+    this.scene.add( spotLightHelper );
 
 
     //軸ヘルパー
@@ -177,8 +204,8 @@ class ThreeApp {
     this.gridHelper = new THREE.GridHelper(100,50);
     this.scene.add(this.gridHelper);
 
-    this.lighthelper = new THREE.DirectionalLightHelper( this.directionalLight, 1 );
-    this.scene.add( this.lighthelper );
+    // this.lighthelper = new THREE.DirectionalLightHelper( this.directionalLight, 1 );
+    // this.scene.add( this.lighthelper );
 
 
 
@@ -189,101 +216,41 @@ class ThreeApp {
 
 
     // // オブジェクト
+
+
+
+
     this.geometry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
+
     // this.material = new THREE.MeshBasicMaterial(ThreeApp.MATERIAL_PARAM);
     this.material = new THREE.MeshPhongMaterial(ThreeApp.MATERIAL_PARAM);
-    // this.box = new THREE.Mesh(this.geometry, this.material);
-    // this.scene.add(this.box);
+    // this.material = new THREE.MeshStandardMaterial(ThreeApp.MATERIAL_PARAM);
+    this.box = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.box);
 
 
-    const Count = 100;
-    this.boxArray = [];
-    // const transformScale = 5.0;
-    const transformScale = 1.0;
-     for (let i = 0; i < Count; ++i) {
-      const box = new THREE.Mesh(this.geometry, this.material);
-      // box.position.z = (Math.random() * 2.0 - 1.0) * transformScale;
-      box.position.y = i * transformScale;
-      this.scene.add(box);
-      //   this.scene.add(torus);
-      this.boxArray.push(box);
-     }
+    this.planeGeo = new THREE.PlaneGeometry( 100, 100, 50, 50 );
+    this.plane = new THREE.Mesh(this.planeGeo, this.material);
+    this.scene.add(this.plane);
+    this.plane.rotation.set( -Math.PI/2, 0, 0 )
 
-
-    // const torusCount = 100;
-    // const transformScale = 5.0;
-    // // this.torusGeometry = new THREE.TorusGeometry(0.5, 0.2, 8, 16);
-    // this.geometry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
-    // this.torusArray = [];
-    // for (let i = 0; i < torusCount; ++i) {
-    //   // トーラスメッシュのインスタンスを生成
-    //   const torus = new THREE.Mesh(this.geometry, this.material);
-    //   // box = new THREE.Mesh(this.geometry, this.material);
-    //   // 座標をランダムに散らす
-    //   // torus.position.x = (Math.random() * 2.0 - 1.0) * transformScale;
-    //   // torus.position.y = (Math.random() * 2.0 - 1.0) * transformScale;
-    //   // torus.position.z = (Math.random() * 2.0 - 1.0) * transformScale;
-
-
-
-    //   torus.position.z = (Math.random() * 2.0 - 1.0) * transformScale;
-    //   // シーンに追加する
-    //   this.scene.add(torus);
-    //   // 配列に入れておく
-    //   this.torusArray.push(torus);
-    // }
-
+    this.clock = new THREE.Clock();
   }
 
 
-
-  // render() {
-  //   requestAnimationFrame(this.render);
-
-  //   // コントロールを更新
-  //   this.controls.update();
-
-  //   this.renderer.render(this.scene, this.camera);
-  // }
 
 
   render() {
     requestAnimationFrame(this.render);
 
-    // マウスの動きに基づいて速度と位置を更新
-    const moveFactorX = 0.1; // 左右の動きの強さ（調整可能）
-
-    // 速度の更新（マウスの位置と現在の位置の差分に基づく）
-    // console.log(_mouseX);
-    // this.vx += (_mouseX - this.x) * moveFactorX;   // x軸の速度を計算
-    // this.vx *= this.friction;                      // 摩擦を適用して減速
-    // this.x += this.vx;                             // 位置を更新
-
-    // // 更新した位置をカメラやオブジェクトに適用
-    // this.box.position.x = this.x;
+    const time = this.clock.getElapsedTime();
+    // console.log(time);
 
 
-
-    for (let i = 0; i < this.boxArray.length; i++) {
-      // console.log(this.boxArray[i]);
-      // console.log(_mouseX);
-      this.vx += (_mouseX - this.x) * moveFactorX;   // x軸の速度を計算
-      this.vx *= this.friction;                      // 摩擦を適用して減速
-      this.x += this.vx;
-      this.boxArray[i].position.x = this.x;
-      // this.boxArray[i].position.x = (Math.random() * 2.0 - 1.0) * this.x;
-      // this.boxArray[i].position.y = this.x;
-      // this.boxArray[i].position.z = (Math.random() * 2.0 - 1.0) * this.x;
-    }
-
-    // this.box.position.x = this.x;
-
-
-
-
-    // this.box.position.y = -_mouseY * 0.1 + 2.0; // Y軸はそのままマウスで調整
-    // this.box.lookAt(this.scene.position);       // 常にシーンの中心を見つめる
-
+    this.box.position.y = 1 * (Math.sin(time * .4) + 1) + 1
+    this.box.rotation.x += .01
+    this.box.rotation.y -= .008
+    this.box.rotation.z -= .005
     // コントロールを更新
     this.controls.update();
 
